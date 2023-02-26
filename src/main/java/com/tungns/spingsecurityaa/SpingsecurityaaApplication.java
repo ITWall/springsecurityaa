7ff8be893bf1a3903b0ac98f1c2d43c15add0bc4
TungNS <nguyentungit95@gmail.com>
2023-02-26 12:59:59 +0700

    init

---
+++ src/main/java/com/tungns/spingsecurityaa/SpingsecurityaaApplication.java
@@ -0,0 +1,13 @@
+package com.tungns.spingsecurityaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingsecurityaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingsecurityaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tungns/spingsecurityaa/SpingsecurityaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tungns.spingsecurityaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingsecurityaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
